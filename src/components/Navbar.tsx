601865711617b33ddc2fbcc859fe7195e3212da6
Mohammed Naushad <mnaushad@catchpoint.com>
2025-09-20 15:54:28 +0530

    Minor update

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,10 +6,10 @@ const Navbar: React.FC = () => {
   return (
     <nav className="navbar">
       <div className="nav-inner">
-        <div className="brand"><Link to="/">MdNaushad</Link></div>
+        <div className="brand"><Link to="/">Md Naushad</Link></div>
         <ul className="nav-links">
           <li><Link to="/">Home</Link></li>
-          <li><a href="https://www.linkedin.com/in/your-profile" target="_blank" rel="noreferrer">LinkedIn</a></li>
+          <li><a href="https://www.linkedin.com/in/mdnaushad15/" target="_blank" rel="noreferrer">LinkedIn</a></li>
           <li><Link to="/resume">Resume</Link></li>
           <li><Link to="/blog">Blog</Link></li>
           <li><Link to="/personal">Personal</Link></li>
